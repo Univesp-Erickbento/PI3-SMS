78ca127459029d3df0b799433e43cd3ccd4d2087
Univesp-ErickBento <erickBento.univesp@gmail.com>
2022-10-20 11:36:44 -0300

    criação da pagina login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './componentes/pages/home/home.component';
+import { LoginComponent } from './componentes/pages/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
