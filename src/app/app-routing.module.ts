2b3a0ef2bfc4fcf3de2b229362856c47c5c6f540
Univesp-ErickBento <erickBento.univesp@gmail.com>
2022-10-20 13:17:47 -0300

    projeto iniciado com a navegação de pagina

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SensoresComponent } from './componentes/cadastros/sensores/sensores.component';
+import { UsuarioComponent } from './componentes/cadastros/usuario/usuario.component';
+import { HomeComponent } from './componentes/pages/home/home.component';
+import { LoginComponent } from './componentes/pages/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: LoginComponent},
+  {path:'home', component: HomeComponent},
+  {path:'cadastro-usuario', component: UsuarioComponent},
+  {path:'cadastro-sensores', component: SensoresComponent}
+];
 
 @NgModule({
+  declarations:[],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
